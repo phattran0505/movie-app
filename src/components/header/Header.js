09b57db7306a54fa42dd4762c9b4a47d1af690a5
phattran <phattran052004@gmail.com>
2024-07-17 21:43:05 +0700

    change title and logo website

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,33 +1,38 @@
-import { Link } from "react-router-dom";
-import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+import { Link } from "react-router-dom";
+
+import "./Header.css";
 function Header() {
+
   return (
     <div className="header">
-        <div className="header-left">
-            <Link to="/" style={{textDecoration:"none"}}>
-                <img
-                    className="header_icon"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"
-                    alt="pics"
-                />
-            </Link>
-            <Link to="/movies/popular" style={{textDecoration:"none"}}>
-                <span className="link">popular</span>
-            </Link>
-            <Link to="/movies/top_rated" style={{textDecoration:"none"}}>
-                <span className="link">Top Rated</span>
-            </Link>
-            <Link to="/movies/upcoming" style={{textDecoration:"none"}}>
-                <span className="link">Upcoming</span>
-            </Link>
+      <div className="header-left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <img
+            className="header_icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"
+            alt="pics"
+          />
+        </Link>
+      </div>
+      <div className="header-right">
+        <div className="links">
+          <Link to="/movies/popular" style={{ textDecoration: "none" }}>
+            <span className="link">popular</span>
+          </Link>
+          <Link to="/movies/top_rated" style={{ textDecoration: "none" }}>
+            <span className="link">Top Rated</span>
+          </Link>
+          <Link to="/movies/upcoming" style={{ textDecoration: "none" }}>
+            <span className="link">Upcoming</span>
+          </Link>
         </div>
-        <div className="header-right">
-            <Link to="/profile">
-                <FontAwesomeIcon className="user-icon" icon={faUser}/>
-            </Link>
-        </div>
+        <Link to="/profile" className="user">
+          <FontAwesomeIcon className="user-icon" icon={faUser} />
+        </Link>
+      </div>
     </div>
   );
 }
--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -4,33 +4,33 @@ import { useParams } from "react-router-dom";
 import Cards from "../card/card";
 import "./movieList.css";
 function MovieList() {
-    const [movieList,setMovieList]=useState([]);
-    const {type}=useParams();
-    useEffect(()=>{
-        getData();
-    },[]);
-
-    useEffect(()=>{
-        getData();
-    },[type]);
-    const getData=()=>{
-        fetch(`https://api.themoviedb.org/3/movie/${type?type:"popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
-            .then (res => res.json())
-            .then (data => setMovieList(data.results))
-    }
-    return ( 
-        <div className="movie__list">
-            <div className="title">{(type?type:"popular").toUpperCase()}</div>
-            <div className="list">
-                {
-                    movieList.map((movie)=>(
-                        <Cards movie={movie}/>
-                    ))
-                }
-            </div>
-        </div>
-
-    );
+  const [movieList, setMovieList] = useState([]);
+  const { type } = useParams();
+  useEffect(() => {
+    getData();
+  }, []);
+  useEffect(() => {
+    getData();
+  }, [type]);
+  const getData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        type ? type : "popular"
+      }?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((data) => setMovieList(data.results));
+  };
+  return (
+    <div className="movie__list">
+      <div className="title">{(type ? type : "popular").toUpperCase()}</div>
+      <div className="list">
+        {movieList.map((movie) => (
+          <Cards movie={movie} key={movie.id}/>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default MovieList;
